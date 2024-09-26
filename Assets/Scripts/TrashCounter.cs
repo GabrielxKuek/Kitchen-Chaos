fc651c59a0f05b9a9b69947412ca95bdd7bbc1d0
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-26 23:23:46 +0800

    Create TrashCounter.cs

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{ 
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
