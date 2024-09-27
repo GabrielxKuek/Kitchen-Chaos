4012a7bc51d4e959f9a3fc10d7490267682f0c37
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-27 18:55:00 +0800

    plate world ui icons
    hard to tell what is on a burger, dynamically adds icons on top of the plate whenever a new ingredient is added

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateIconsSingleUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO)
+    {
+        image.sprite = kitchenObjectSO.sprite;
+    }
+}
