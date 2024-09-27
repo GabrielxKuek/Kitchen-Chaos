b9554c89acc93ac28dae258ff626db8ab1203ffe
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-27 15:17:15 +0800

    working code for platecounter, created the appropriate assets for plates and made plates stack on top of platecounter

---
+++ Assets/Scripts/PlatesCounter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatesCounter : BaseCounter
+{ // spawns plates every set amount of time
+
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private float spawnPlateTimerMax = 4f;
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spawnPlateTimer;
+    private int platesSpawnedAmount;
+    private int platesSpawnedMax = 4;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax)
+        {
+            //KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+            spawnPlateTimer = 0f;
+
+            if (platesSpawnedAmount < platesSpawnedMax)
+            {
+                platesSpawnedAmount += 1;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            // player is empty handed
+            if (platesSpawnedAmount > 0)
+            {
+                // theres at least one plate here
+                platesSpawnedAmount -= 1;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
