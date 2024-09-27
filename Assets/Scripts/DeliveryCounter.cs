081df821cbf4cdc314f70bb4477fa22f7d4e4e5b
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-27 20:17:41 +0800

    deliverycounter, changes to scene

---
+++ Assets/Scripts/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                // only accepts plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
