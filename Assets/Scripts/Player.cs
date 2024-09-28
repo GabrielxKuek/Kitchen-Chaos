f012e8955f2adfb268cacc76b78a343f35aa9f1e
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-28 13:00:05 +0800

    gamestart, statemanager for game states, countdown ui
    game states
    - WaitingToStart
    - CountdownToStart
    - GamePlaying
    - GameOver

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,6 +50,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
@@ -58,6 +60,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
