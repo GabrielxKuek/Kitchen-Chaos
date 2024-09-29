8e662ba451c4517bde3a91888c9e191d13edc501
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-09-29 19:04:29 +0800

    options menu to configure audio levels
    player preferences for audio volume is saved using unity's PlayerPrefs class

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    private const string PLAYER_PREFS_MUSIC_VOLUME = "MusicVolume";
+    private float volume = .3f;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        audioSource = GetComponent<AudioSource>();
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_MUSIC_VOLUME, .3f);
+        audioSource.volume = volume;
+    }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        audioSource.volume = volume;
+        PlayerPrefs.SetFloat(PLAYER_PREFS_MUSIC_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+}
